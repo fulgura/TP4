45571ed630044ac5d43986bfb36f7da8c8da423e
Diego Martín Correa <diegomartincorrea@gmail.com>
2011-12-04 23:41:03 -0300

    Iterando y buscando las dos mejores y las dos peores reglas

--- Entrega/ejercicio1.m
+++ Entrega/ejercicio1.m
@@ -30,14 +30,34 @@ Datos = [2,2,2,1;
 
 
 %% Generamos una problacion valida
-Pop = GenerarPoblacion(lchrom, popsize)
+Pop = GenerarPoblacion(lchrom, popsize);
 
 
 %% Evaluamos las reglas donde el consecuente es aprobado.
 % calculo el soporte y la confianza para cada reglas
-Soporte = CalcularSoporte(Datos, Pop) %sobre aprobados
+Soporte = CalcularSoporte(Datos, Pop); %sobre aprobados
+Confianza = CalcularConfianza(Datos, Pop, NroClase);
+% Medida de aptitud : promedio entre el soporte y la confianza
+Fitness = (Soporte + Confianza) ./2
 
-Confianza = CalcularConfianza(Datos, Pop, NroClase)
+%% Iteraciones 
+
+for ite = 1 : 3
+    
+    
+    [OrdenFitness Indices] = sort(Fitness, 2, 'descend');
+    
+    %% Elegimos las dos mejores reglas
+    regla1 = Pop(:, Indices(1))
+    regla2 = Pop(:, Indices(2))
+    
+    
+    %% Elegimos las dos peores
+        % 5. Elijo las 2 peores reglas
+    peor1 = Pop(:, Indices(popsize))
+    peor2 = Pop(:, Indices(popsize - 1))
+    
+end
 
 
 
